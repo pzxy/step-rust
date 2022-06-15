63a857964b9e12ca790cf99595a4d48148df8df3
pzxy <wonkung@163.com>
2022-06-15 22:32:02 +0800

    pattern

--- basics/src/pattern.rs
+++ basics/src/pattern.rs
@@ -72,4 +72,174 @@ fn pattern7() {
     }
 }
 
+// 8. 模式的匹配
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+fn pattern8() {
+    let a = 1;
+    match a {
+        // | 匹配多种
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        // 匹配 4 到 8 的值
+        // 这种写法还可以匹配字符
+        // 'a' ..='c' => println!("a b c d"),
+        4..=8 => println!("4 5 6 7 8"),
+        _ => println!("anything"),
+    }
+}
+
+// 9. 解构 分解值
+fn pattern9() {
+    let p = Point {
+        x: 1,
+        y: 2,
+    };
+    // 解构 结构体,这里相当于这样写
+    // let Point { x: x, y: y } = p;
+    let Point { x, y } = p;
+    assert_eq!(0, 1);
+    assert_eq!(0, 2);
+    // match 匹配结构体,这里挺好的,因为可以
+    match p {
+        Point { x, y: 0 } => println!("x:{},y:{}", x, y),
+        Point { x: 0, y } => println!("x:{},y:{}", x, y),
+        Point { x, y } => println!("x:{},y:{}", x, y),
+    }
+    // 解构元组 ,只要对应好就能都取出来, 非常好用
+    let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+}
+
+// 10 match 匹配 枚举enum
+enum Color {
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(Color)
+}
+
+fn pattern10() {
+    let msg = Message::ChangeColor(Color::Rgb(0, 160, 255));
+    match msg {
+        Message::Quit => {
+            println!("quit")
+        }
+        Message::Move { x, y } => {
+            println!("move")
+        }
+        // 这里特别像 java 中得 lambda 表达式,text 作为匿名函数的入参
+        Message::Write(text) => {
+            println!("text");
+        }
+        // 一个一个对应取出来
+        Message::ChangeColor(Color::Rgb(r, g, b)) => {
+            println!("{},{},{}", r, g, b);
+        }
+        Message::ChangeColor(Color::Hsv(r, g, b)) => {
+            println!("{},{},{}", r, g, b);
+        }
+        // 或者使用 _ =>(),
+    }
+}
+
+// 11 使用 _ 忽略值
+fn pattern11() {
+    // 忽略参数,这里其实第一个入参传的值会被忽略
+    // fn f1(_:i32,y:i32)
+    let s = Some(5);
+    match s {
+        // 这里其实只要是一个 Some 就可以了,具体是什么值就忽略了他
+        Some(_) => {}
+        _ => {}
+    }
+    // 忽略值的某一部分,比如匹配的时候
+    let number = (1, 2, 3, 4, 5);
+    match number {
+        // 用不到的就忽略
+        (one, _, three, _, five) => {
+            println!("{},{},{}", one, three, five)
+        }
+    }
+    // 忽略未使用的变量
+    let _x = 5;
+    // 奇技淫巧
+    let s2 = Some(String::from("bear"));
+    // 这里Some(_s),或者别的字母,是不行的,后面的打印会报错,
+    // 因为会移交所有权,导致最后的打印没有所有权
+    // 这里使用了 let,会获取所有权的,使用_可以不获取.
+    if let Some(_) = s2 {
+        println!(" hero");
+    }
+    println!("{:?}", s2)
+}
+
+// 12 使用 .. 忽略剩余部分
+
+fn pattern12() {
+    let s = Point { x: 0, y: 3 };
+    match s {
+        // 这里使用 .. 来省略后面的变量,如果后面有 5 个,就会省略 5 个
+        Point { x, .. } => println!("x:{}", x)
+    }
+    let num = (1, 2, 4, 4, 1, 19);
+    match num {
+        // 这里省略中间的所有,
+        // 但是不能发现歧义,比如   (..,two,..) 这样
+        (one, .., last) => {
+            println!("one:{},last:{}", one, last)
+        }
+    }
+}
+
+// 13, 使用 match 守卫,来提供额外的条件.
+// match 守卫就是 match arm 模式后额外的 if 判断条件,想要匹配条件也必须满足
+fn pattern13() {
+    let x = Some(5);
+    let y = 10;
+    match x {
+        Some(50) | Some(40) => println!("Got 50"),
+        Some(n) if n == y => println!("match ,n={:?}", n),
+        _ => println!("ignore"),
+    }
+    println!("{:?},{}", x, y);
+    let flag = false;
+    match 10 {
+        // 匹配数字
+        4 | 5 | 6 if flag == true => println!("yes"),
+        _ => println!("no")
+    }
+}
+
+// 14 @ 创建一个变量,匹配后绑定匹配值
+fn pattern14() {
+    let msg = Point { x: 5, y: 19 };
+    match msg {
+        // 匹配指定值
+        Point { x: 1, y: 2 } => {
+            // 这种匹配指定值的,是没有办法将变量取出来的
+            // println!("x:{},y:{}", x, y)
+            println!("1, 2")
+        }
+        // 限定范围,注意@使用,
+        // 因为使用了范围,所以要获取值需要创建一个新的变量
+        Point { x: tmp @ 1..=10, y: _, } => {
+            println!("tmp:{}", tmp)
+        }
+        // 匹配任意值,
+        // 也可以写 Point { x, y, } =>{println!("x:{},y:{}", x, x)}
+        Point { x: a, y: b, } => {
+            println!("x:{},y:{}", a, b)
+        }
+    }
+}
+
+
 
