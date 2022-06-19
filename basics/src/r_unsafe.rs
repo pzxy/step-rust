dc46f7071dd26f418997d5bc724765961323c2e3
pzxy <wonkung@163.com>
2022-06-19 22:32:06 +0800

    unsafe

--- basics/src/r_unsafe.rs
+++ basics/src/r_unsafe.rs
@@ -5,6 +5,11 @@
 // 但是 rust 为了安全,还是会杀掉.
 // - rust 需要能够进行底层的系统编程.
 
+// 何时使用 unsafe 代码?
+// 编译器无法保证内存安全,保证 unsafe 代码正确并不简单
+// 有充足理由使用 unsafe 代码时,就可以这样做.
+// 通过显示标记 unsafe,可以在出现问题是轻松定定位.
+
 // 1. unsafe的超能力
 // 使用 unsafe 关键字来切换到 unsafe Rust,开启一个块,里面放着 unsafe 代码.
 // unsafe rust 里可执行四个动作(unsafe 超能力)
@@ -106,3 +111,79 @@ pub fn unsafe4() {
     //   slice::from_raw_parts_mut(r,10000)
     // };
 }
+
+// 5. 使用 extern 函数调用外部代码,类似 go 中 import "C"
+// extern 关键字:简化创建和使用外部函数接口 FFI 的过程.
+// 外部函数接口(FFI,Foreign Function Interface):它允许一种编程语言定义函数,
+// 并让其他变成语言能调用这些函数
+extern "C" {
+    // 这里的 C 是指明的应用二进制接口 ABI(Application Binary Interface),它是定义函数在汇编层面的调用方式的
+    // "C" ABI是常见的 ABI,它遵循 C 语言的 ABI
+    // 任何在 extern 中定义的语言都是不安全的
+    // 因为 rust 不会对他进行检查
+    fn abs(input: i32) -> i32;
+}
+
+fn unsafe5() {
+    unsafe {
+        println!("{}", abs(-3));
+    }
+}
+
+// 6. 从其他语言调用 rust 函数
+// 可以说使用 extern 创建接口,其他语言铜鼓哦他们可以调用 Rust 的函数
+// fn 前添加 extern 关键字,并指定 ABI
+// 还需添加#[no_mangle]注解,避免 Rust 在编译时改变它的名称.
+
+#[no_mangle]
+// 这种类型调用时,不需要使用 unsafe
+pub extern "C" fn call_from_c() {
+    println!("call_from_c 在编译和链接后可以被 C 语言访问")
+}
+
+// 7. 访问或修改一个可变静态变量
+// Rust 支持全局变量,但因为所有权机制可能产生某些问题,例如数据竞争
+// 在 rust 里,全局变量叫做静态(static)变量
+
+// 静态变量与常量类似
+// 命名规范: acreaming_snake_case, 大写,下划线
+// 必须标注类型
+// 静态变量只能存储 static 声明周期的引用,无需显示标注.因为能够推断出来
+
+// 常量与不可变静态变量的区别
+// 静态变量:有固定的内存你地址,使用他的值总会访问同样的数据
+// 常量:允许使用他们的时候对数据进行赋值.
+// 静态变量:可以是可变的,访问和修改静态变量是不安全的
+static HELLO_WORLD: &str = "hello,world";
+// 声明一个可变的静态变量
+static mut COUNTER: u32 = 0;
+
+// 做一层包装
+fn add_to_count(inc: u32) {
+    // 这里要放到 unsafe 不安全代码块中,
+    // 因为修改和访问可变静态变量是不安全的操作
+    unsafe {
+        COUNTER += inc;
+    }
+}
+
+fn unsafe7() {
+    println!("{}", HELLO_WORLD);
+    add_to_count(3);
+    // 我们访问这个可变的静态变量也要放到 unsafe 代码块中.
+    // 因为可能有多线程的情况,出现数据竞争,尽可能用前面学过的只能指针来做.
+    unsafe {
+        println!(" COUNTER:{}", COUNTER);
+    }
+}
+
+// 8. 实现不安全的 (unsafe) trait
+// 当某个 trait 中至少一个方法拥有编译器无法校验的不安全因素是,就称这个 trait 是不安全的.
+// 声明 unsafe trait:定义前加 unsafe 关键字
+// - 该 trait 只能在 unsafe 代码块中实现
+unsafe trait Foo {}
+
+// 不安全的 trait 要在 unsafe 的 代码块中进行实现
+unsafe impl Foo for i32 {}
+
+
