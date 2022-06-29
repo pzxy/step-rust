d16d8ade979b1853e1ccfbad31ec55807d19c22c
王坤 <tielin.wang@tuya.com>
2022-06-29 14:00:40 +0800

    reverse print

---
+++ algorithms/leetcode/src/offer06_reverse_print.rs
@@ -0,0 +1,31 @@
+
+
+//输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+//
+//
+//
+// 示例 1：
+//
+// 输入：head = [1,3,2]
+// 输出：[2,3,1]
+
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+//
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+
+pub fn reverse_print(head: Option<Box<ListNode>>) -> Vec<i32> {
+
+}
