ea9850c422efb739cb8477da1943222aff43efae
pzxy <wonkung@163.com>
2022-07-23 14:45:36 +0800

    kv-server

---
+++ practice/kv-server/tests/integration_test.rs
@@ -0,0 +1,14 @@
+use crate::common::setup;
+
+//导入本目录下的模块
+mod common;
+
+// 这里没有写cfg是因为，tests目录会被特殊对待
+// 只有执行cargo  tests 时候这个目录下的文件才会被编译。
+#[test]
+fn demo() {
+    // 只能调用lib.rs文件下的pub方法
+    // 或者tests目录下的其他模块，通过mod引用进来，来调用。
+
+    setup();
+}
