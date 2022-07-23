668829e76b8bb39b09ed0db1f364d71ba6fae3ca
pzxy <wonkung@163.com>
2022-07-23 22:05:26 +0800

    httpserver

---
+++ practice/http-server/src/main.rs
@@ -0,0 +1,39 @@
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::{fs, thread};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        thread::spawn(|| handle_connection(stream));
+        // println!("Connection established!");
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    let get = b"GET / HTTP/1.1\r\n"; // b 转换成自己字符串
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+    let context = fs::read_to_string(filename).unwrap();
+    let response = format!("{}{}", status_line, context);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
+// println!("Request:{}", String::from_utf8_lossy(&buffer[..]));
+// 请求
+// Method Request-URI HTTP-Version CRLF
+// header CRLF
+// message-body
+
+// 响应
+// HTTP-Version Status-Code Reason-Phrase CRLF
+// header CRLF
+// message-body
--- practice/kv-server/tests/common/mod.rs
+++
@@ -1,6 +0,0 @@
-// 这里虽然也是一个文件，但是不是一个crate，只有tests目录下的一级文件才是crate
-// 可以创建一个方法给其他测试crate使用
-
-pub fn setup() {
-    println!("setup")
-}
