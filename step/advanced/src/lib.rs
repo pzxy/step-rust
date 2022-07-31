2a30fd90253dc9ea306bf98aa25c9616097ee513
pzxy <wonkung@163.com>
2022-08-01 06:45:40 +0800

    async wait

--- step/advanced/src/lib.rs
+++ step/advanced/src/lib.rs
@@ -1,8 +1,17 @@
+mod async_wait;
+
 #[cfg(test)]
 mod tests {
+    use crate::async_wait::async_main;
+    use futures::executor::block_on;
+
     #[test]
     fn it_works() {
         let result = 2 + 2;
         assert_eq!(result, 4);
+    }
+    #[test]
+    fn async_wait() {
+        block_on(async_main());
     }
 }
