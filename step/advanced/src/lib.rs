562dc6d951fbdcfc0e345dbd81ebb9a1f503859f
王坤 <tielin.wang@tuya.com>
2022-08-05 13:44:18 +0800

    delete tokio file

--- step/advanced/src/lib.rs
+++ step/advanced/src/lib.rs
@@ -1,33 +1,29 @@
+use crate::logger::log1;
+use crate::tracing::trance1;
+
 mod async_wait;
 pub mod logger;
 mod tracing;
-mod tokio;
-
-use crate::logger::log1;
-use crate::tracing::trance1;
 
 pub fn log_demo() {
     // log1()
     trance1()
 }
+
 #[cfg(test)]
 mod tests {
-    use crate::async_wait::async_main;
     use futures::executor::block_on;
-    use crate::tokio::tokio1;
+
+    use crate::async_wait::async_main;
 
     #[test]
     fn it_works() {
         let result = 2 + 2;
         assert_eq!(result, 4);
     }
+
     #[test]
     fn async_wait() {
         block_on(async_main());
-    }
-
-    #[test]
-    fn test_tokio(){
-         tokio1()
     }
 }
