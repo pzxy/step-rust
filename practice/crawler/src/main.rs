226ec01a126ed33ef5cdbcb896c76dbaa4b6511c
pzxy <wonkung@163.com>
2022-08-13 17:39:29 +0800

    json

--- practice/crawler/src/main.rs
+++ practice/crawler/src/main.rs
@@ -1,28 +1,33 @@
-use std::fs::File;
-use std::io::Write;
+use log::info;
+use serde::{Deserialize, Serialize};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
     // 爬谁?
-    let client = reqwest::Client::new();
-    let res = client.get("https://solution.tuya.com/api/v2/market/searchList?pageNo=12&pageSize=10")
-        .send().await?;
-    // 正则
-    let resp = res.text().await?;
-
-    let mut f = File::create("data.json")?;
-    let _ = f.write(resp.as_ref());
+    let res = reqwest::get("https://solution.tuya.com/api/v2/market/searchList?pageNo=1&pageSize=100").await?.text().await?;
+    let resp: Root = serde_json::from_str(res.as_str())?;
+    if resp.success == true {
+        for v in resp.result.datas {
+            println!("{} : {}", v.solution_name, v.head_line)
+        }
+    }
+    // 存储
     Ok(())
 }
 
-struct Struct1 {
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Struct2 {
     pub code: String,
     pub level: i64,
     pub name: String,
     pub category_id: i64,
 }
 
-struct Struct {
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Struct1 {
     pub on_shelf: i64,
     pub solution_name: String,
     pub sort: i64,
@@ -30,18 +35,22 @@ struct Struct {
     pub solution_code: String,
     pub head_line: String,
     pub main_image: String,
-    pub category_list: Vec<Struct1>,
+    pub category_list: Vec<Struct2>,
     pub solution_id: i64,
     pub solution_type: i64,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 struct Result {
-    pub datas: Vec<Struct>,
+    pub datas: Vec<Struct1>,
     pub page_no: i64,
     pub search_title: String,
     pub total_count: i64,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 struct Root {
     pub result: Result,
     pub t: i64,
