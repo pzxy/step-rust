21baa4c52199c687285b852c63644d89fb6ec730
pzxy <wonkung@163.com>
2022-10-29 13:52:44 +0800

    fix build_pcre

--- practice/rust-ffi/src/build_pcre.sh
+++ practice/rust-ffi/src/build_pcre.sh
@@ -2,8 +2,21 @@ set -e
 if [ ! -d ./pcre2-10.40 ]; then
   unzip pcre2-10.40.zip
 fi
+
 pwd=$(pwd)
-cd ./pcre2-10.40 &&
-./configure --prefix="${pwd}"/pcre &&
-make && make install &&
+cd ./pcre2-10.40 && ./configure --prefix="${pwd}"/pcre && make && make install
+
+file_name="../pcre/include/pcre2.h"
+# pcre的unit width配置
+context="#define PCRE2_CODE_UNIT_WIDTH 8"
+if [ $(uname) = "Darwin" ]; then
+  sed -i '' '1 i\
+'"${context}"'
+' ${file_name}
+else
+  sed -i '1i '"${context}"'
+' ${file_name}
+fi
+
+
 
