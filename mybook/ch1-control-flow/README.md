59fe5bce62ea77b28a43c0c3f5bc09ad86911a07
pzxy <wonkung@163.com>
2022-11-07 21:32:39 +0800

    control flow

---
+++ mybook/ch1-control-flow/README.md
@@ -0,0 +1,4 @@
+# 基本的控制流
+
+基本语法 关键字 声明等。let if for match while等。
+
--- mybook/ch1-control-flow/src/main.rs
+++ mybook/ch1-control-flow/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    // 声明，rust中的声明全部都是 变量名：类型
+    // 无论是作为函数参数，还是函数中的临时变量。
+    // 这里的let表示一种绑定，将0这个值的所有权绑定到变量s上。
+    let s: u8 = 0; //变量名：类型
+                   // 这种s..写法其实很有意思，会一直增加，直到u8类型溢出panic。
+    for i in s.. {
+        println!("Hello, world! {}", i);
+    }
+    // 还有 if let 这种写法
+    let v = match s {
+        // if 这种
+        s if (s > 0) => 1,
+        _ => s,
+    };
+    //while 循环和 loop循环，基本上主要就是这些控制流了。以后补充
 }
