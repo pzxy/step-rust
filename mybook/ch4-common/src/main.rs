61df248236dde174a130f0fe1e91c52d3f811ea6
pzxy <wonkung@163.com>
2022-11-08 21:51:19 +0800

    args

--- mybook/ch4-common/src/main.rs
+++ mybook/ch4-common/src/main.rs
@@ -1,8 +1,8 @@
 use ch4_common::signal_demo;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let input = args.get(1);
+    // let args: Vec<String> = std::env::args().collect();
+    let input = std::env::args().nth(1);
     println!("input:{:?}", input);
     signal_demo()
 }
