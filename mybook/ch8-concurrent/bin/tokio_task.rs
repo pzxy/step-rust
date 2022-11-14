5ddee062caf445fcfd015b1dcabd116a5a101f23
pzxy <wonkung@163.com>
2022-11-14 15:52:01 +0800

    tokio

--- mybook/ch8-concurrent/bin/tokio_task.rs
+++ mybook/ch8-concurrent/bin/tokio_task.rs
@@ -1,39 +1,62 @@
 use std::thread::sleep;
 use std::time::Duration;
+use tokio::time;
 
 #[tokio::main]
 async fn main() {
     // tokio::spawn 相当于go关键字
     tokio::spawn(async {
-        // 这种其实是异步编程，这里面的是顺序执行的。
+        // 一定会先执行hello1 再执行hello2
+        // 如果他们中有future返回pending则会让出线程。
         hello1().await;
         hello2().await
     });
-    hello3().await
+    hello3().await;
 }
 
 async fn hello1() {
     for i in 1..5 {
-        sleep(Duration::from_secs(1));
-        println!("hello1--{}", i)
+        println!("hello1--{}", i);
     }
+    // 这里返回pending，这里会让出线程。
+    time::sleep(Duration::from_secs(2)).await;
 }
 
 async fn hello2() {
     for i in 1..5 {
-        sleep(Duration::from_secs(1));
         println!("hello2=={}", i)
     }
 }
 
 async fn hello3() {
     for i in 1..5 {
+        println!("hello3**{}", i);
         sleep(Duration::from_secs(1));
-        println!("hello3**{}", i)
     }
 }
+// 执行结果：
+// hello3**1
+// hello1--1
+// hello1--2
+// hello1--3
+// hello1--4
+// hello3**2
+// hello2==1
+// hello2==2
+// hello2==3
+// hello2==4
+// hello3**3
+// hello3**4
 
-// 可以看到发生了异步
+// 执行结果分析：
+// 1. hello2一定在hello1后面，因为在同一个future中(async {})。执行顺序是一定的。
+// 2. 这里未必是hello3先执行，看hello1和hello3哪个先准备好。
+// 3. hello1一定是连续打印完，然后hello3再执行。因为hello1执行完会sleep1秒。这时sleep future会返回pending，会暂时让出线程。
+// 由于hello2要2秒后才能执行，所以hello3会抢占住线程。然后执行，hello3执行两次后刚好2秒过去，这时hello2开始执行。
+// 这里hello3的睡眠是线程的sleep，是不会返回pending的，那为什么hello2能抓住时机执行呢？那是因为hello2和hello3在不同的future中。
+// hello2和hello3属于不同的task，task其实就可以看成是go中的协程了。
+// 因为go是自己来实现调度器的，所以他更抽象，直接让我们将协程当成线程的概念用了。我们不必关系里面的异步模型是什么。但是rust我们需要了解的。
+
 // 易错点：
 // 1. hello3 不能也用 tokio::spawn(async { hello3().await });
 // 这样会直接退出的，因为主线程直接结束了。和go中一样，go全部逻辑都开协程，也是会退出的。
@@ -50,3 +73,7 @@ async fn hello3() {
 //             let rc = Rc::new("hello");
 //             println!("{}", rc);
 //         }
+// 5. thread::sleep 和 tokio::time::sleep有什么区别？
+// thread::sleep 会继续计算，不回让出线程让别的future执行。
+// tokio::time::sleep会让立即返回，让出线程让别的future执行。等sleep时间到后，sleep future会调用wake，
+// 通知调度器重新执行自己的poll方法,这次调用因为时间已经到了，所以会立即返程成功，然后往下执行。
