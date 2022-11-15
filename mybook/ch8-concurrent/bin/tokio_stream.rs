c901cd7c3c32983804d3ae58bd8816fbdce75d21
pzxy <wonkung@163.com>
2022-11-15 21:08:11 +0800

    tokio stream

---
+++ mybook/ch8-concurrent/bin/tokio_stream.rs
@@ -0,0 +1,15 @@
+use futures::TryStreamExt;
+/// tokio-stream 可以让迭代器在迭代时异步进行。
+/// 依赖  tokio-stream = { version = "0.1" }
+/// stream 没搞明白，有点困了
+use tokio_stream::StreamExt;
+
+#[tokio::main]
+async fn main() {
+    let mut stream = tokio_stream::iter(&[1, 2, 3]);
+    // stream上调用next() 需要被pin
+    tokio::pin!(stream);
+    while let Some(v) = stream.next().await {
+        println!("GOT = {:?}", v);
+    }
+}
