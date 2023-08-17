bf121fae6d7cda7e7311cb159ffdfb79e012af0d
kwin <wonkung@163.com>
2023-08-17 22:15:25 +0800

    chore: review code

--- demo/examples/5_anagram2.rs
+++ demo/examples/5_anagram2.rs
@@ -16,6 +16,22 @@ pub fn anagrams_for<'a>(word: &'a str, possible_anagrams: &'a [&'a str]) -> Hash
     HashSet::from_iter(r)
 }
 
+pub fn anagrams_for2<'a>(word: &'a str, possible_anagrams: &'a [&'a str]) -> HashSet<&'a str> {
+    let low = word.to_lowercase();
+    let mut low_s = low.chars().collect::<Vec<_>>();
+    low_s.sort();
+    let r = possible_anagrams
+        .iter()
+        .filter(|p| {
+            let low_p = p.to_lowercase();
+            let mut cs = low_p.chars().collect::<Vec<_>>();
+            cs.sort();
+            cs == low_s && low != low_p
+        })
+        .copied();
+    HashSet::from_iter(r)
+}
+
 fn process_anagram_case(word: &str, inputs: &[&str], expected: &[&str]) {
     let result = anagrams_for(word, inputs);
     let expected: HashSet<&str> = expected.iter().cloned().collect();
