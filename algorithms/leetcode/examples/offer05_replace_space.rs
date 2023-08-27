143ccac971be02d3433a5df7a5695321da969319
kwin <wonkung@163.com>
2023-08-27 23:44:57 +0800

    chore: replace space

--- algorithms/leetcode/examples/offer05_replace_space.rs
+++ algorithms/leetcode/examples/offer05_replace_space.rs
@@ -2,11 +2,12 @@
 // 例如： "we are happy"
 // 输出:  "we%20are%20happy"
 
-fn main(){
-    let ret = replace_space("we are happy".to_string());
-    println!("{}", ret);
+fn main() {
+    let ret = replace_space2("we are happy".to_string());
+    println!("ret: {}", ret);
     assert_eq!(ret, "we%20are%20happy")
 }
+
 pub fn replace_space(s: String) -> String {
     let mut ret = String::from("");
     for c in s.chars() {
@@ -19,6 +20,15 @@ pub fn replace_space(s: String) -> String {
         }
     }
     ret
+}
+
+pub fn replace_space2(s: String) -> String {
+    return s.chars().map(|c|
+        if c == ' ' {
+            "%20".to_string()
+        } else {
+            c.to_string()
+        }).collect::<Vec<String>>().join("");
 }
 
 // fn replace_space2(s: String) -> String {
